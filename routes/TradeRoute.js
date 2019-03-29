a74a82aeac7ee4717ee16cc0dd914113503539e2
hieungo <ngophanquanghieu@gmail.com>
2019-03-29 12:26:35 +0700

    save message to room

--- routes/TradeRoute.js
+++ routes/TradeRoute.js
@@ -2,7 +2,9 @@ var trading = require('../controller/TradeController');
 
 module.exports = function (app) {
    //user send message
-    app.route('/trading')
+   app.route('/trading')
       .get(trading.getUserTrading)
       .post(trading.createTrade);
+   app.route('/trading/msg')
+      .get(trading.getRoomMessage);
 }
